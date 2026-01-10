7bc5b4d4b3102422084a7119ef468fc455a22db7
Cyb3R0xSASA <elmohtarftop@gmail.com>
2026-01-10 04:47:30 +0200

    chore: allow english & arabic texts

--- src/utils/filltext.ts
+++ src/utils/filltext.ts
@@ -1,6 +1,7 @@
 import { PDFName, PDFNull } from "pdf-lib";
 import type { IDrawText, IFillText, IRemoveBG } from "../types/index.js";
 import wrapText from "./wraptext.js";
+import getMajorityDirection from "./identifyChars.js";
 
 const removeBG = ({ field, pdf }: IRemoveBG): void => {
     try {
@@ -12,15 +13,14 @@ const removeBG = ({ field, pdf }: IRemoveBG): void => {
     }
 }
 
-const drawText = (width: number, y: number, x: number, height: number, { text, page, color, fontSize, font, maxWidth, fontHeight = 22, coords, center }: IDrawText) => {
+const drawText = (width: number, y: number, x: number, height: number, { text, page, color, fontSize, font, maxWidth, fontHeight = 22, coords, center, dir }: IDrawText) => {
     const lines = wrapText({ fontSize, text: text, font, maxWidth });
 
     let counter = 0;
     for (const line of lines) {
         const textWidth = font.widthOfTextAtSize(line, fontSize);
-        const coordX = center ? (width - textWidth) / 2 : coords.x ? x + maxWidth - textWidth + coords.x : x + maxWidth - textWidth;
+        const coordX = center ? (width - textWidth) / 2 : dir === 'ltr' ? x : coords.x ? x + maxWidth - textWidth + coords.x : x + maxWidth - textWidth;
         const coordY = counter === 0 ? (coords.y ? coords.y + y : y) : (coords.y ? coords.y + height : height);
-        console.log(text, coordX, center);
 
         page.drawText(line, {
             x: coordX,
@@ -42,14 +42,14 @@ const fillText = ({ pdf, field, text, page, color, fontSize, font, maxWidth, fon
         const { width } = page.getSize()
         let height = y;
 
-
         if (typeof text === 'string') {
             drawText(width, y, x, height, { text, page, color, fontSize, font, maxWidth, fontHeight, coords, center })
         } else {
             for (const tx of text) {
-                drawText(width, y, x, height, { text: tx, page, color, fontSize, font, maxWidth, fontHeight, coords, center })
-                height -= 2;
-                y = height
+                const dir = getMajorityDirection(tx);
+                drawText(width, y, x, height, { text: tx, page, color, fontSize, font, maxWidth, fontHeight: 8, coords, center, dir })
+                height -= dir === 'ltr' ? 20 : 10;
+                y = height + 2
             }
         }
     }
